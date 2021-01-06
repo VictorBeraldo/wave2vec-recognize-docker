aec0c7d718c0c670c37fdef3d5ea40396e610881
Victor Beraldo <9519566+VictorBeraldo@users.noreply.github.com>
2021-01-06 21:09:36 -0200

    add add dict letters

---
+++ data/run_recognize.sh
@@ -0,0 +1,2 @@
+
+python examples/wav2vec/recognize.py --target_dict_path=/app/data/dict.ltr.txt /app/data/wav2vec_small_10m.pt /app/data/test.wav
--- run_container.sh
+++ run_container.sh
@@ -1,1 +1,1 @@
-docker container run -it --rm -v $(pwd)/data:data --name vicosbe-fairseq-container vicosbe-fairseq-image
+docker container run -it --rm -v ${pwd}/data:/python/data --name vicosbe-fairseq-container vicosbe-fairseq-image
